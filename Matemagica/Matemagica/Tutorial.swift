c140072368016deec551d80d99204ac883e18e8a
ricardohochman <ricardohochman@hotmail.com>
2015-05-20 17:38:31 -0300

    sprite kit implementado na classe tutorial

--- Matemagica/Matemagica/Tutorial.swift
+++ Matemagica/Matemagica/Tutorial.swift
@@ -10,115 +10,131 @@
 import SpriteKit
 
 class Tutorial: SKScene {
-
+    
     var tag:Int!
-
-    override init(size: CGSize) {
-        super.init(size: size)
-        
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    var vC: TutorialDetailViewController!
+    var lblTitulo: SKLabelNode = SKLabelNode()
+    
+    
+    
     
     override func didMoveToView(view: SKView) {
         backgroundColor = SKColor.whiteColor()
-
-        // declaração da label conta
-        var lblTitulo = SKLabelNode()
+        
         lblTitulo.fontName = "Noteworthy-Bold"
         lblTitulo.fontSize = 100
-//        lblTitulo.position = CGPoint(x: self.size.width * 0.5, y: self.size.height * 0.2)
-        lblTitulo.text = "4dashuidasuiusiadhsuadiu "
+        lblTitulo.position = CGPoint(x: size.width * 0.5, y: size.height * 0.8)
         lblTitulo.fontColor = SKColor.blackColor()
-        lblTitulo.position = CGPoint(x: size.width/2, y: size.height/2)
-        lblTitulo.zPosition = 100000000000
-
         addChild(lblTitulo)
-        println("\(lblTitulo.text)")
         
-//        UILabel(frame: CGRectMake(0, 0, self.view!.frame.size.width * 0.8, self.view!.frame.height * 0.2))
-//        lblTitulo.adjustsFontSizeToFitWidth = true
-//        lblTitulo.font = UIFont(name: "Noteworthy-Bold", size: 100)
-//        lblTitulo.textAlignment = NSTextAlignment.Center
-//        lblTitulo.center = CGPointMake(self.view!.frame.size.width * 0.5, self.view!.frame.size.height * 0.2)
-//        lblTitulo.text = "4 + 5"
-//
-//        self.scene?.view!.addSubview(lblTitulo)
-//
-//        // declaração da label conta
-//        var lblConta = UILabel(frame: CGRectMake(0, 0, self.view!.frame.size.width * 0.5, self.view!.frame.height * 0.5))
-//        lblConta.adjustsFontSizeToFitWidth = true
-//        lblConta.numberOfLines = 3
-//        lblConta.font = UIFont(name: "Noteworthy-Bold", size: 100)
-//        lblConta.textAlignment = NSTextAlignment.Center
-//        lblConta.center = CGPointMake(self.view.frame.size.width * 0.25, self.view.frame.size.height * 0.5)
-//        self.view.addSubview(lblConta)
-//        
         switch tag {
         case 1:
-            println("hfahafsuihfuaihiauh")
-            println("fooooooiiiiiii =D")
-//            self.navigationItem.title = "SOMA"
+            vC.navigationItem.title = "SOMA"
             lblTitulo.text = "4 + 5"
-            var numero1Img: UIImage = UIImage(named: "4")!
-            let imageView1 = UIImageView(image: numero1Img)
-            imageView1.frame = CGRectMake(self.view!.frame.size.width * 0.2, self.view!.frame.size.height * 0.35, self.view!.frame.size.width * 0.1, self.view!.frame.size.height * 0.15)
-            self.view!.addSubview(imageView1)
             
-            var numero2Img: UIImage = UIImage(named: "5")!
-            let imageView2 = UIImageView(image: numero2Img)
-            imageView2.frame = CGRectMake(self.view!.frame.size.width * 0.2, self.view!.frame.size.height * 0.55, self.view!.frame.size.width * 0.1, self.view!.frame.size.height * 0.15)
-            self.view!.addSubview(imageView2)
+            var numero1Img: SKSpriteNode = SKSpriteNode(imageNamed: "4")
+            numero1Img.position = CGPoint(x: size.width * 0.25, y: size.height * 0.65)
+            numero1Img.size = CGSize(width: size.width * 0.1, height: size.height * 0.15)
+            addChild(numero1Img)
+
             
-            var numero3Img: UIImage = UIImage(named: "9")!
-            let imageView3 = UIImageView(image: numero3Img)
-            imageView3.frame = CGRectMake(self.view!.frame.size.width * 0.2, self.view!.frame.size.height * 0.75, self.view!.frame.size.width * 0.1, self.view!.frame.size.height * 0.15)
-            self.view!.addSubview(imageView3)
+            var numero2Img: SKSpriteNode = SKSpriteNode(imageNamed: "5")
+            numero2Img.position = CGPoint(x: size.width * 0.25, y: size.height * 0.45)
+            numero2Img.size = CGSize(width: size.width * 0.1, height: size.height * 0.15)
+            addChild(numero2Img)
+            
+            var ref = CGPathCreateMutable()
+            var linha = SKShapeNode()
+            
+            CGPathMoveToPoint(ref, nil, size.width * 0.05, size.height * 0.325)
+            CGPathAddLineToPoint(ref, nil, size.width * 0.37, size.height * 0.325)
+            linha.path = ref
+            linha.lineWidth = 8
+            linha.fillColor = UIColor.blackColor()
+            linha.strokeColor = UIColor.blackColor()
+            addChild(linha)
+
+            
+            var numero3Img: SKSpriteNode = SKSpriteNode(imageNamed: "9")
+            numero3Img.position = CGPoint(x: size.width * 0.25, y: size.height * 0.20)
+            numero3Img.size = CGSize(width: size.width * 0.1, height: size.height * 0.15)
+            addChild(numero3Img)
+
+            
+            
+            
+            for var i = 0; i < 4; i++ {
+                let exemplo1Img = SKSpriteNode(imageNamed: "4")
+                
+                exemplo1Img.physicsBody = SKPhysicsBody(rectangleOfSize: exemplo1Img.frame.size)
+                exemplo1Img.physicsBody!.dynamic = false
+                
+                var xPos1 = (CGFloat(self.size.width * 2) * CGFloat(Double(i) / 20) + CGFloat(self.size.width))
+                println(xPos1)
+                
+                exemplo1Img.position = CGPointMake(xPos1, numero1Img.position.y)
+                exemplo1Img.size = CGSize(width: size.width * 0.1, height: size.height * 0.15)
+                self.addChild(exemplo1Img)
+                
+                
+                let actionMove = SKAction.moveBy(CGVector(dx: -self.size.width / 2, dy: 0.0), duration: 3.0)
+                exemplo1Img.runAction(SKAction.sequence([actionMove]))
+                
+                
+            }
+            
+            
+            for var i = 0; i < 5; i++ {
+                let exemplo2Img = SKSpriteNode(imageNamed: "5")
+                
+                exemplo2Img.physicsBody = SKPhysicsBody(rectangleOfSize: exemplo2Img.frame.size)
+                exemplo2Img.physicsBody!.dynamic = false
+                
+                var xPos2 = (CGFloat(self.size.width * 2) * CGFloat(Double(i) / 20) + CGFloat(self.size.width))
+                println(xPos2)
+                
+                exemplo2Img.position = CGPointMake(xPos2, numero2Img.position.y)
+                exemplo2Img.size = CGSize(width: size.width * 0.1, height: size.height * 0.15)
+                self.addChild(exemplo2Img)
+                
+                
+                let actionMove = SKAction.moveBy(CGVector(dx: -self.size.width / 2, dy: 0.0), duration: 3.0)
+                exemplo2Img.runAction(SKAction.sequence([actionMove]))
+                
+                
+            }
+            
             
 //            var dist = 0.1
 //            for var i = 0; i < 4; i++ {
 //                
-//                //                let delayTime3 = dispatch_time(DISPATCH_TIME_NOW, Int64(2 * Double(NSEC_PER_SEC)))
-//                //                dispatch_after(delayTime3, dispatch_get_main_queue()) {
 //                var exemplo1Img: UIImage = UIImage(named: "4")!
 //                let imageViewEx1 = UIImageView(image: exemplo1Img)
-//                imageViewEx1.frame = CGRectMake(self.view.frame.size.width * CGFloat(0.3 + dist), self.view.frame.size.height * 0.35, self.view.frame.size.width * 0.1, self.view.frame.size.height * 0.15)
+//                imageViewEx1.frame = CGRectMake(self.view!.frame.size.width * CGFloat(0.3 + dist), self.view!.frame.size.height * 0.35, self.view!.frame.size.width * 0.1, self.view!.frame.size.height * 0.15)
 //                imageViewEx1.alpha = 0.0
-//                self.view.addSubview(imageViewEx1)
+////                self.view!.addSubview(imageViewEx1)
 //                
 //                UIView.animateWithDuration(2, animations: { () -> Void in
 //                    
 //                    imageViewEx1.alpha = 1.0
 //                })
 //                
-//                //                    }
 //                dist += 0.1
 //            }
             
-
+            
+        case 2:
+            vC.navigationItem.title = "SUBTRAÇÃO"
+            lblTitulo.text = "8 - 3"
+        case 3:
+            vC.navigationItem.title = "MULTIPLICAÇÃO"
+            lblTitulo.text = "2 × 3"
+        case 4:
+            vC.navigationItem.title = "DIVISÃO"
+            lblTitulo.text = "10 ÷ 5"
         default:
             break
         }
-//        if tag == 1 {
-//        }
-//        if buttonTag == 2 {
-//            self.navigationItem.title = "SUBTRAÇÃO"
-//            lblTitulo.text = "Vamos aprender a subtrair?"
-//        }
-//        if buttonTag == 3 {
-//            self.navigationItem.title = "MULTIPLICAÇÃO"
-//            lblTitulo.text = "Vamos aprender a multiplicar?"
-//        }
-//        if buttonTag == 4 {
-//            self.navigationItem.title = "DIVISÃO"
-//            lblTitulo.text = "Vamos aprender a dividir?"
-//        }
-
     }
-    
-    
-    
     
 }
