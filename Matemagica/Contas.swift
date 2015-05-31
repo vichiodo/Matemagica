0d6448ef54a910c86d71ea4c0f0064dc6503d258
PatriciaAbreu <PatriciaAbreu@MacBook-Pro-de-Patricia.local>
2015-05-31 20:01:35 -0300

    mudança das variáveis classe Contas

--- Matemagica/Contas.swift
+++ Matemagica/Contas.swift
@@ -10,23 +10,24 @@ import UIKit
 
 
 class Contas {
-    var conta: String!
-    var alternativa1: Int!
-    var alternativa2: Int!
-    var alternativa3: Int!
-    var resposta: Int!
+    var account: String!
+    var alternative1: Int!
+    var alternative2: Int!
+    var alternative3: Int!
+    var answer: Int!
     
-    var numeros: Array<Int> = [1,2,3,4,5,6,7,8,9,10]
+    var numbers: Array<Int> = [1,2,3,4,5,6,7,8,9,10]
     
     init(operacao: String){
-        obterOperacoes(operacao)
+        getOperator(operacao)
     }
 
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
     //obtem as operações de acordo com a String passada e retorna o resultado da operação
-    func obterOperacoes(operador: String) -> (resultado: Int, operacao: String) {
+    func getOperator(operador: String) -> (result: Int, operation: String) {
         var n1: Int = 0
         var n2: Int = 0
         
@@ -34,87 +35,87 @@ class Contas {
         n1 = random(0, 50)
         n2 = random(0, 50)
         
-        var operacao: String = ""
+        var operation: String = ""
         
         //imprime na tela as operações
         if(operador == "+"){
-            operacao = " \(n1) + \(n2)"
+            operation = " \(n1) + \(n2)"
         }else if operador == "-"{
             if n1 > n2{
-                operacao = " \(n1) - \(n2)"
+                operation = " \(n1) - \(n2)"
             }else {
-                operacao = " \(n2) - \(n1)"
+                operation = " \(n2) - \(n1)"
             }
         }else if operador == "*" {
             n1 = random(1, 10)
             n2 = random(1, 10)
-            operacao = " \(n1) × \(n2)"
+            operation = " \(n1) × \(n2)"
             
         }else if operador == "/" {
             
             var array = random(0, 9)
-            n2 = numeros[array]
+            n2 = numbers[array]
             
             do{
                 n1 = random(n2, 20)
             }while n1 % n2 != 0
             
-            operacao = "\(n1) ÷ \(n2)"
+            operation = "\(n1) ÷ \(n2)"
         }
         
-        conta = operacao
+        account = operation
         
-        return (resolveOperacao(n1, n2: n2, op: operador), operacao)
+        return (solveOperation(n1, n2: n2, op: operador), operation)
     }
     
     //resolve as operações
-    func resolveOperacao(n1: Int, n2: Int, op: String) -> Int{
-        var resultado: Int = 0
+    func solveOperation(n1: Int, n2: Int, op: String) -> Int{
+        var result: Int = 0
         if op == "+" {
-            resultado = n1 + n2
+            result = n1 + n2
         }else if op == "-" {
             if n1 > n2 {
-                resultado = n1 - n2
+                result = n1 - n2
             }else{
-                resultado = n2 - n1
+                result = n2 - n1
             }
         }else if op == "*" {
-            resultado = n1 * n2
+            result = n1 * n2
         }else if op == "/" {
-            resultado = n1 / n2
+            result = n1 / n2
         }
         
-        resposta = resultado
-        addAlternativas()
-        return resultado
+        answer = result
+        addAlternatives()
+        return result
     }
     
-    func addAlternativas(){
+    func addAlternatives(){
 
         var num1 = 0
         var num2 = 0
         var num3 = 0
         
-        num1 = random(resposta - 20, resposta - 1)
-        num2 = random(resposta + 1, resposta + 20)
-        num3 = random(resposta + 3, resposta + 24)
+        num1 = random(answer - 20, answer - 1)
+        num2 = random(answer + 1, answer + 20)
+        num3 = random(answer + 3, answer + 24)
         
         if num1 < 0 {
             num1 = num1 * (-1) + 2
         }
-        if num1 == resposta || num1 == num2 {
+        if num1 == answer || num1 == num2 {
             num1++
         }
-        if num2 == resposta || num2 == num3 {
+        if num2 == answer || num2 == num3 {
             num2++
         }
-        if num3 == resposta || num3 == num1 {
+        if num3 == answer || num3 == num1 {
             num3++
         }
         
-        alternativa1 = num1
-        alternativa2 = num2
-        alternativa3 = num3
+        alternative1 = num1
+        alternative2 = num2
+        alternative3 = num3
         
     }
 }
